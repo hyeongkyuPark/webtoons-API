200499988555f934e02780d33a35c06621a2661e
parkhyeongkyu <phg0644@gmail.com>
2020-08-11 08:56:50 +0900

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const port = 3000;
 
-app.listen(port, () => {
-    console.log("Express listning on",port);
+app.listen(process.env.PORT || port, () => {
+    console.log("Express listning on");
 });
